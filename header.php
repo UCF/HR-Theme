f8a0675a0293cd93807b67903260bd17a4b1fa7d
Jo Greybill <jogreybill@gmail.com>
2012-09-07 15:48:02 -0400

    Basic header changes to accomodate search form

--- header.php
+++ header.php
@@ -53,21 +53,10 @@
 		<div class="container">
 			<div class="row">
 				<div id="header" class="row-border-bottom-top">
-					<h1><a href="<?=bloginfo('url')?>"><?=bloginfo('name')?></a></h1>
-					<?php $options = get_option(THEME_OPTIONS_NAME);?>
-					<?php if($options['facebook_url'] or $options['twitter_url']):?>
-					<ul class="social menu horizontal span3">
-						<?php if($options['facebook_url']):?>
-						<li><a class="ignore-external facebook" href="<?=$options['facebook_url']?>">Facebook</a></li>
-						<?php endif;?>
-						<?php if($options['twitter_url']):?>
-						<li><a class="ignore-external twitter" href="<?=$options['twitter_url']?>">Twitter</a></li>
-						<?php endif;?>
-					</ul>
-					<?php else:?>
-					<div class="social span3">&nbsp;</div>
-					<?php endif;?>
-					<div class="end"><!-- --></div>
+					<h1 class="span8"><a href="<?=bloginfo('url')?>"><?=bloginfo('name')?></a></h1>
+					<div class="span4" id="header-search">
+						<?php get_search_form();?>
+					</div>
 				</div>
 			</div>
 			<?=wp_nav_menu(array(
