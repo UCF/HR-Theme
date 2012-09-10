41532e325e4f362c75868398861375e0b0a2e8fe
Jo Greybill <jogreybill@gmail.com>
2012-09-10 12:27:41 -0400

    Updated footer contact info to use theme settings info

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<?php $options = get_option(THEME_OPTIONS_NAME);?>
 			<div id="footer">
 				
 				<?=wp_nav_menu(array(
@@ -11,24 +12,30 @@
 					));
 				?>
 				<div class="row" id="footer-widget-wrap">
-					<div class="footer-widget-1 span3">
+					<div class="footer-widget-1 span6">
 						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column One')):?>
-							<a class="ignore-external" href="http://www.ucf.edu"><img src="<?=THEME_IMG_URL?>/logo.png" alt="" title="" /></a>
+							<a class="ignore-external" href="http://www.ucf.edu"><img id="footer-logo" src="<?=THEME_IMG_URL?>/logo.png" alt="" title="" /></a>
+							
+							<div class="footer-contact">
+								<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?>
+								<?php if($options['organization_name']) { print '<strong>'.$options['organization_name'].'</strong><br/>'; } ?>
+								<?php if($options['organization_addr_str']) { print $options['organization_addr_str'].'<br/>'; } ?>
+								<?php if($options['organization_addr_str_2']) { print $options['organization_addr_str_2'].'<br/>'; } ?>
+								<?php if($options['organization_addr_csz']) { print $options['organization_addr_csz'].'<br/>'; } ?>
+								<?php if($options['organization_phone']) { 
+									print $options['organization_phone'].'<br/>'; } ?>
+								<?php endif;?>
+							</div>
+							
 						<?php endif;?>
 					</div>
 					<div class="footer-widget-2 span3">
-						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Two')):?>
-						&nbsp;
-						<?php endif;?>
-					</div>
-					<div class="footer-widget-3 span3">
 						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Three')):?>
 						&nbsp;
 						<?php endif;?>
 					</div>
-					<div class="footer-widget-4 span3">
+					<div class="footer-widget-3 span3">
 						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Footer - Column Four')):?>
-							<?php $options = get_option(THEME_OPTIONS_NAME);?>
 							<?php if($options['site_contact'] or $options['organization_name']):?>
 								<div class="maintained">
 									Site maintained by the <br />
