cd52d5b00a3ae1ad788f8733f13aeecd7c47f480
Jo Greybill <jogreybill@gmail.com>
2012-09-11 15:52:33 -0400

    Added sidebar content; updated one- and two-col templates, style fixes

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,19 @@
 <?php disallow_direct_load('sidebar.php');?>
 
 <?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Sidebar')):?>
+
+	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_title; ?>
+	
+	<p class="post-search-header">Search This Section</p>
+	<?=do_shortcode('[post-type-search post_type_name="document" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1"]')?>
+			
+	&nbsp;
+	<a class="faq-link" href="#">Benefits and Retirement FAQs</a>
+			
+	&nbsp;
+	<?=do_shortcode('[site-contact-email]')?>
+			
+	&nbsp;
+	<?=do_shortcode('[site-contact-info]')?>
+
 <?php endif;?>
--- template-one-column.php
+++ template-one-column.php
@@ -5,11 +5,17 @@
 ?>
 <?php get_header(); the_post();?>
 	<div class="row page-content" id="<?=$post->post_name?>">
+		<div class="span12" id="page-top">
+			<? if(!is_front_page())	{ ?>
+				<h1><?php the_title();?></h1>
+			<? } ?>
+			<?php if (get_post_meta($post->ID, 'page_subheader', TRUE)) {
+				print get_post_meta($post->ID, 'page_subheader', TRUE);
+			}
+			?>
+		</div>
 		<div class="span12">
 			<article>
-				<? if(!is_front_page())	{ ?>
-						<h1><?php the_title();?></h1>
-				<? } ?>
 				<?php the_content();?>
 			</article>
 		</div>
