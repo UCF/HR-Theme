1f53fe76511c8822f135c209e00cff34da5323b6
Jo Greybill <jogreybill@gmail.com>
2012-09-11 10:04:13 -0400

    Added PgSections as a taxonomy for Documents

--- custom-post-types.php
+++ custom-post-types.php
@@ -255,7 +255,8 @@ class Document extends CustomPostType{
 		$use_title      = True,
 		$use_editor     = False,
 		$use_shortcode  = True,
-		$use_metabox    = True;
+		$use_metabox    = True,
+		$taxonomies     = array('post_tag', 'pg_sections');
 	
 	public function fields(){
 		$fields   = parent::fields();
