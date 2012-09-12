8bb75aeea5d4de509063a119518a84b545837fc7
Jo Greybill <jogreybill@gmail.com>
2012-09-12 09:11:15 -0400

    Altered shortcode to show empty pg_sections

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_title; ?>
 	
 	<p class="post-search-header">Search This Section</p>
-	<?=do_shortcode('[post-type-search post_type_name="document" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1"]')?>
+	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true]')?>
 			
 	&nbsp;
 	<a class="faq-link" href="#">Benefits and Retirement FAQs</a>
