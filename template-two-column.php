1a4753f1df9a727330bfd4b4e22b11271873b2c3
Jo Greybill <jogreybill@gmail.com>
2012-09-14 10:10:42 -0400

    Added breadcrumbs to one, two-col templates

--- template-two-column.php
+++ template-two-column.php
@@ -6,9 +6,12 @@
 <?php get_header(); the_post();?>
 	<div class="row page-content" id="<?=$post->post_name?>">
 		<div class="span12" id="page-top">
+			<?=get_breadcrumbs($post->ID)?>
+			
 			<? if(!is_front_page())	{ ?>
 				<h1><?php the_title();?></h1>
 			<? } ?>
+			
 			<?php if (get_post_meta($post->ID, 'page_subheader', TRUE)) {
 				print get_post_meta($post->ID, 'page_subheader', TRUE);
 			}
