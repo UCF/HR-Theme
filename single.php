b1c2f5714aad9821484a12b762034f1749c90a1c
Jo Greybill <jogreybill@gmail.com>
2012-09-14 10:34:19 -0400

    Updated single post template to include correct header structure with breadcrumbs

--- single.php
+++ single.php
@@ -2,17 +2,17 @@
 <?php get_header(); the_post();?>
 	
 	<div class="row page-content" id="<?=$post->post_name?>">
-		<div class="span9">
+		<div class="span12" id="page-top">
+			<?=get_breadcrumbs($post->ID)?>
+			
+			<? if(!is_front_page())	{ ?>
+					<h1><?php the_title();?></h1>
+			<? } ?>
+		</div>
+		<div class="span12">
 			<article>
-				<? if(!is_front_page())	{ ?>
-						<h1><?php the_title();?></h1>
-				<? } ?>
 				<?php the_content();?>
 			</article>
-		</div>
-		
-		<div id="sidebar" class="span3">
-			<?=get_sidebar();?>
 		</div>
 	</div>
 		
