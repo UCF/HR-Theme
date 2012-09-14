0a4116497f87b39145a9f1e8188c16b8d4865435
Jo Greybill <jogreybill@gmail.com>
2012-09-14 13:07:40 -0400

    Updated default post search input field text

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
 	<?php $taxonomy_term = get_post_meta($post->ID, 'page_taxonomy_term', TRUE) ? get_post_meta($post->ID, 'page_taxonomy_term', TRUE) : $post->post_name.'-page'; ?>
 	
 	<p class="post-search-header">Search This Section</p>
-	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true]')?>
+	<?=do_shortcode('[post-type-search post_type_name="resourcelink" taxonomy="pg_sections" taxonomy_term="'.$taxonomy_term.'" column_width="span3" column_count="1" show_empty_sections=true default_search_text="Find a Resource"]')?>
 			
 	&nbsp;
 	<a class="faq-link" href="<?=get_site_url()?>/current-employees/benefits-faqs/">Benefits and Retirement FAQs</a>
