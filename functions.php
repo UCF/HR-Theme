fab07befb3b84fba19aa95c665e8d710619a18ff
Jo Greybill <jogreybill@gmail.com>
2013-01-03 09:54:54 -0500

    Added Enable Media Replace plugin files to theme

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ require_once('functions/admin.php');  			# Admin/login functions
 require_once('functions/config.php');			# Where per theme settings are registered
 require_once('shortcodes.php');         		# Per theme shortcodes
 require_once('third-party/truncate-html.php');  # Includes truncateHtml function
+require_once('third-party/enable-media-replace/enable-media-replace.php');  # Includes Enable Media Replace plugin (does not register plugin in WP!)
 
 //Add theme-specific functions here.
 
