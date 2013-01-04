e0775c660d51da9843b1fdecaf64d6c8d512ac12
Jo Greybill <jogreybill@gmail.com>
2013-01-04 13:02:27 -0500

    Re-added single-resourcelink with redirect rules so that site search results link to the correct content

---
+++ single-resourcelink.php
@@ -0,0 +1,36 @@
+<?php disallow_direct_load('single-document.php');?>
+<?php 
+the_post();
+
+$link_url 			= get_post_meta($post->ID, 'resourcelink_url', TRUE);
+$existing_page 		= get_post_meta($post->ID, 'resourcelink_page', TRUE);
+$existing_page_url 	= get_permalink($existing_page);
+$attachment_id  	= get_post_meta($post->ID, 'resourcelink_file', TRUE);
+
+if ($attachment_id) {
+	$attachment_url = wp_get_attachment_url($attachment_id);
+	$url = $attachment_url;
+}
+
+// Fallback link redirects
+// (see ResourceLinks::get_url() in custom-post-types.php)
+elseif ($link_url) {
+	// If it's a page anchor (starts with #), set up the URL accordingly
+	if (substr($link_url, 0) == '#') {
+		$url = $existing_page_url.$link_url;
+	}
+	else {
+		$url = $link_url;
+	}
+}
+elseif ($link_url == '' && $existing_page !== '') {
+	$url = $existing_page_url;
+}
+// Absolute fallback to home page
+else {
+	$url = get_site_url();
+}
+
+
+header('Location: '.$url);
+?>
