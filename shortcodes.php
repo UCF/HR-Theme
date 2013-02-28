3346a25708143f96cea17dc8a5499a46ee9d084c
Jo Greybill <jogreybill@gmail.com>
2013-02-28 15:07:32 -0500

    Updated theme help; added taxonomy_term sorting so that sections are always alphabetized whether or not a particular taxonomy term is specified

--- shortcodes.php
+++ shortcodes.php
@@ -286,6 +286,26 @@ function sc_post_type_search($params=array(), $content='') {
 	if ($params['taxonomy_term'] !== '') {
 		// if a specific taxonomy term is specified, get just its children
 		$termchildren = get_term_children(get_term_by('slug', $params['taxonomy_term'], $params['taxonomy'])->term_id, $params['taxonomy']);
+		
+		$termchildren_byname = array();
+		$termchildren_sorted = array();
+		
+		// Create new array that contains term ID and Name data per term
+		foreach ($termchildren as $termid) {
+			$term = get_term_by('id', $termid, $params['taxonomy']);
+			$termchildren_byname[$term->slug] .= $termid;
+		}
+		
+		// Sort the $sorted_termchildren results by Name
+		ksort($termchildren_byname);			
+		
+		// Replace $termchildren with the newly sorted results
+		foreach ($termchildren_byname as $termname => $termid) {
+			$termchildren_sorted[] .= $termid;
+		}
+		$termchildren = $termchildren_sorted;
+		
+		
 		foreach ($termchildren as $term) {
 			$args['tax_query'][0]['terms'] = $term;
 			$posts = get_posts($args);
@@ -297,8 +317,10 @@ function sc_post_type_search($params=array(), $content='') {
 			}
 		}
 	}
+	// If no taxonomy_term is specified, grab all taxonomy terms
+	// for the given taxonomy
 	else {
-		foreach(get_terms($params['taxonomy']) as $term) {
+		foreach(get_terms($params['taxonomy']) as $term) { // get_terms defaults to an orderby=name, order=asc value
 			$args['tax_query'][0]['terms'] = $term->term_id;
 			$posts = get_posts($args);
 	
