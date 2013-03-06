dc5e47ad712fea9a03014863e7a9cf7150a1f861
Roger Wolf <roger.wolf@ucf.edu>
2013-03-06 18:06:53 -0500

    changed default placeholder text in site search field

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?=home_url( '/' )?>">
 	<div>
 		<label for="s">I'm looking for</label>
-		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Enter your search term here..." />
+		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Search HR" />
 	</div>
 </form>
