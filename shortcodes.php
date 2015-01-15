089f12a76fab06d846e018f1c5529ca6630fc67d
Jim Barnes <james.barnes@ucf.edu>
2015-01-15 16:52:34 -0500

    Updated the way uncategorized items show up in the search-post-type shortcode.

--- shortcodes.php
+++ shortcodes.php
@@ -427,17 +427,33 @@ function sc_post_type_search($params=array(), $content='') {
 			<? $count = 0; ?>
 			<? foreach($section as $section_title => $section_posts) { ?>
 				<? if ($section_posts) { ?>
-					<? 	if ($count % $params['column_count'] == 0 && $count !== 0) {
-						print '</div><div class="row">';
-					} ?>
-					<div class="<?=$params['column_width']?>">
-						<h3><?=esc_html($section_title)?></h3>
-						<ul>
-						<? foreach(array_slice($section_posts, $start, $end) as $post) { ?>
-							<li data-post-id="<?=$post->ID?>" <?=($post_type->get_document_application($post)) ? 'class="'.$post_type->get_document_application($post).'"' : ''?>><?=$post_type->toHTML($post)?><span class="search-post-pgsection"><?=$section_title?></span></li>
-						<? } ?>
-						</ul>
-					</div>
+					<?  if ($section_title == $params['uncategorized_term_name'] ) { ?>
+						</div>
+						<div class="row">
+							<div class="span12">
+								<div class="uncategorized-posts">
+									<h3><?=esc_html($section_title); ?></h3>
+									<ul>
+									<? foreach(array_slice($section_posts, $start, $end) as $post) { ?>
+										<li data-post-id="<?=$post->ID?>" <?=($post_type->get_document_application($post)) ? 'class="'.$post_type->get_document_application($post).'"' : ''?>><?=$post_type->toHTML($post)?><span class="search-post-pgsection"><?=$section_title?></span></li> 
+									<? } ?>	
+									</ul>							
+								</div>
+							</div>
+						
+					<? } else { ?>
+						<? 	if ($count % $params['column_count'] == 0 && $count !== 0) {
+							print '</div><div class="row">';
+						} ?>
+						<div class="<?=$params['column_width']?>">
+							<h3><?=esc_html($section_title)?></h3>
+							<ul>
+							<? foreach(array_slice($section_posts, $start, $end) as $post) {  ?>
+								<li data-post-id="<?=$post->ID?>" <?=($post_type->get_document_application($post)) ? 'class="'.$post_type->get_document_application($post).'"' : ''?>><?=$post_type->toHTML($post)?><span class="search-post-pgsection"><?=$section_title?></span></li>
+							<? } ?>
+							</ul>
+						</div>
+					<? } //end else ?> 
 				<? $count++; 
 				} // endif ?>
 			<? } // endforeach ?>
